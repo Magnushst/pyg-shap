1c89835294d819e0c58b6ee4f36c5193da5b5695
Magnushst <magnus.henrik@hotmail.com>
2026-03-11 11:51:07 +1000

    Updated README

--- src/pyg_shap/__init__.py
+++ src/pyg_shap/__init__.py
@@ -2,5 +2,5 @@
 from .wrapper import PyGCaptumWrapper
 from .explainer import compute_gat_shap_values
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 __all__ = ["PyGCaptumWrapper", "compute_gat_shap_values"]
